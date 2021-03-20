724c60d2c87a07cf3d3a48c3e2616d09ec4df086
Olivia Lewis <ollybaby12@gmail.com>
2021-03-20 01:42:14 -0400

    create db, seed, config, and service object

---
+++ seeds/seed.bookmarks_content.sql
@@ -0,0 +1,12 @@
+INSERT INTO bookmarks (title, url, rating)
+VALUES
+    ('Amazon', 'https://www.amazon.com/', '5'),
+    ('Google', 'https://www.google.com/', '4'),
+    ('YessStyle', 'https://www.yesstyle.com/en/', '3'),
+    ('InnisFree', 'https://us.innisfree.com/', '4'),
+    ('Youtube', 'https://www.youtube.com/', '5'),
+    ('Hello World', 'https://helloworld.com/', '3'),
+    ('Quick Poses', 'https://quickposes.com/en', '4'),
+    ('Coolors', 'https://coolors.co/', '4'),
+    ('Made This Up', 'https://madethis-up.com/', '3'),
+    ('Not a Real Thing', 'https://noitisnt.com/', '2');
--- src/bookmarks/bookmarks-router.js
+++ src/bookmarks/bookmarks-router.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { v4: uuid } = require('uuid')
 const logger = require('../logger')
 const { bookmarks } = require('../store')
+const BookmarksService = require('./bookmarks-service')
 
 const bookmarksRouter = express.Router()
 const bodyParser = express.json()
